6e65d368f193c3496fb0cacdd8014f9399cfeffa
BugsBunnies1 <brankojaksic91@gmail.com>
2019-07-18 19:03:28 +0200

    Created SimpleApp_v_1_0.py

---
+++ src/SimpleApp_v_1_0.py
@@ -0,0 +1,27 @@
+'''
+#################################################################
+    Task:           Creating a simple app                       #
+                                                                #
+    Description:                                                #
+                    -   Demand for user to enter his/her name   #
+                    -   Output users name                       #
+                                                                #
+    Version:        1.0                                         #
+                                                                #
+    Creator:        BugsBunnies1                                #
+                                                                #
+    Date:           07/18/19                                    #
+#################################################################
+'''
+
+
+# Preprocessor directive
+import os
+
+# Demand for user to enter his/her name
+userName = input("Please enter your name: ")
+
+# Output users name
+print("Hello {}, nice meeting you!!!".format(userName))
+
+
